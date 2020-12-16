d89c403a8e3f6f72724fe5ab35c39c2932941c78
SWARALI <Swarali.Dhongadi@lntinfotech.com>
2020-12-16 11:16:00 +0530

    Class added

---
+++ Hello_Welcome/Hello_Welcome/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Welcome
+{
+    partial class Class1
+    {
+        public void printWelcome()
+        {
+            Console.WriteLine("Welcome to GitHub!");
+        }
+    }
+}
