d7de0f19a70b7965601f114936bb09246c6ab3d0
SWARALI <Swarali.Dhongadi@lntinfotech.com>
2020-12-16 11:05:53 +0530

    Folder added

---
+++ Hello_Welcome/Hello_Welcome/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Welcome
+{
+    class Program
+    {
+        public void sayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void Main(string[] args)
+        {
+            new Program().sayHello();   
+        }
+    }
+}
